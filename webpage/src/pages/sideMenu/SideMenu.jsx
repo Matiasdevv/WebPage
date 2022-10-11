d7b1414b35e23218af1503261a599093d1347949
Matiasdevv <gaunam134@gmail.com>
2022-10-11 09:08:53 -0300

    modificando menu lateral

--- webpage/src/pages/sideMenu/SideMenu.jsx
+++ webpage/src/pages/sideMenu/SideMenu.jsx
@@ -1,20 +1,18 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Acordeones from '../../Components/pure/Acordeones'
 import '../../Styles/sideMenu.scss'
 
 const SideMenu = () => {
 
-    const [sideBar, setSide] = useState(false) //todo: meter al sidebar un estado para poder abrir o cerrar este.
+    const [sideBar, setSide] = useState(true) //todo: meter al sidebar un estado para poder abrir o cerrar este.
 
     const menu = (e)=>{
-        if(e.target.checked){
+        setSide(!sideBar)
+        if(sideBar){
             e.target.parentNode.parentNode.firstElementChild.style.display = 'none'
-            setSide(!sideBar)
-
-
         }else{
             e.target.parentNode.parentNode.firstElementChild.style.display = 'inherit'
-            setSide(!sideBar)
         }
     }
 
@@ -22,31 +20,28 @@ const SideMenu = () => {
   return (
     <div className='sideMenu__container'>
 
-    <div className='sideMenu__container-link'>
-        <Link to={'/page2'}>Page 2</Link>
-        <Link to={'/page3'}>Page 3</Link>
-        <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown button
-            </button>
-            <ul class="dropdown-menu">
-                <li><Link to={'/page1'}>Page 1</Link></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+
+        <div clasname= 'container'>
+
+        <div className='sideMenu__container-link'>
+
+            <Acordeones/>
         </div>
-    </div>
-
-    <div className='sideMenu__container-button'>
-        <input type='checkbox' id='check' onClick={(e)=>menu(e)} hidden/>
-        <label htmlFor='check'>
-            <i className="bi bi-backspace-reverse"></i>
-        </label>
-
-    </div>
 
 
 
+
+        <div className='sideMenu__container-button'>
+            <input type='checkbox' id='check' onClick={(e)=>menu(e)} hidden/>
+            <label htmlFor='check'>
+                <i className="bi bi-backspace-reverse"></i>
+            </label>
+
+        </div>
+
+
+
+        </div>
     </div>
   )
 }
