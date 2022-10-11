9c787d900d9da0ffa7f5ac7d3a6db68cd5588551
Matiasdevv <gaunam134@gmail.com>
2022-10-11 09:07:18 -0300

    agregando archivo para los videos y estilos

---
+++ webpage/src/models/steps.js
@@ -0,0 +1,41 @@
+export const steps = [
+    {
+      label: 'Video #1',
+      video: <iframe width="100%" height="100%" src="https://www.youtube.com/embed/0uVOzkJB-QM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+    },
+    {
+      label: 'Video #2',
+      video:
+      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/rAbZdOvisb8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+    },
+    {
+      label: 'Video #3',
+      video: <iframe width="100%" height="100%" src="https://www.youtube.com/embed/gwM1CNWIXv4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+    },
+    {
+      label: 'Video #1',
+      video: <iframe width="100%" height="100%" src="https://www.youtube.com/embed/0uVOzkJB-QM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+    },
+    {
+      label: 'Video #2',
+      video:
+      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/rAbZdOvisb8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+    },
+    {
+      label: 'Video #3',
+      video: <iframe width="100%" height="100%" src="https://www.youtube.com/embed/gwM1CNWIXv4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+    },
+    {
+      label: 'Video #1',
+      video: <iframe width="100%" height="100%" src="https://www.youtube.com/embed/0uVOzkJB-QM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+    },
+    {
+      label: 'Video #2',
+      video:
+      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/rAbZdOvisb8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+    },
+    {
+      label: 'Video #3',
+      video: <iframe width="100%" height="100%" src="https://www.youtube.com/embed/gwM1CNWIXv4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+    },
+  ]
