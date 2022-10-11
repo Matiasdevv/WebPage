2e93ffb0e915f5f272d63aa6de93c0c0a9a21f57
Matiasdevv <gaunam134@gmail.com>
2022-10-11 09:43:08 -0300

    modificando estilos de dashboard

--- webpage/src/pages/dashboard/Dashboard.jsx
+++ webpage/src/pages/dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link, Route, Router, Routes, useNavigate} from 'react-router-dom'
 import { Home } from '../home/Home'
 import '../../Styles/dasboard.scss'
+import FeedIcon from '@mui/icons-material/Feed';
 
 const Dashboard = () => {
 
@@ -12,21 +13,20 @@ const Dashboard = () => {
 
         <div className='dash-container'>
 
-          <div className='flex-item'>
-            <Link to={'/'} className='logo' ><i className="bi bi-shop"></i></Link>
+          <div className='flex-item logo'>
+            <Link to={'/'} className='logo' title='logo' ><i className="bi bi-shop"></i></Link>
           </div>
 
           <div className='flex-item buscador'>
-            <button className='buscador__boton'><i className="bi bi-search"></i></button>
+            <button className='buscador__boton'><i className="bi bi-search" title='buscar'></i></button>
             <input type={'text'} placeholder={'Buscar...'} className='buscador__text' />
           </div>
 
           <div className='flex-item link-container'>
-                <Link to={'/'} className='link' ><i className="bi bi-house-door"></i></Link>
-                <Link to={'/Sobre-nosotros'} className='link' ><i className="bi bi-people-fill"></i></Link>
+                <Link to={'/'} className='link' title='Página principal'><i className="bi bi-house-door"></i></Link>
+                <Link to={'/Sobre-nosotros'} className='link' title='Sobre nosotros'><i className="bi bi-people-fill"></i></Link>
+                <Link to={'/Prueba'} className='link' title='Realizar prueba' ><FeedIcon/></Link>
           </div>
-
-
 
         </div>
 
