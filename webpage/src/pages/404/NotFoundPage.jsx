fe92d5f7014f6561b21c6aa53788b6e6eed88d28
Matiasdevv <gaunam134@gmail.com>
2022-10-12 17:56:56 -0300

    estilizando pagina 404

--- webpage/src/pages/404/NotFoundPage.jsx
+++ webpage/src/pages/404/NotFoundPage.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const NotFoundPage = () => {
   return (
-    <div>404-NotFoundPage</div>
+    <section>
+
+        <h1>Lo sentimos! esta pagina no ha sido encontrada</h1>
+        <h2>Error 404</h2>
+
+          <img src='../../images/undraw_landscape_photographer_re_vrtu.png' title='404'/>
+
+    </section>
   )
 }
 
