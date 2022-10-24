0cbe913b1704953933bd886a22683fd888c87f19
Matiasdevv <gaunam134@gmail.com>
2022-10-24 13:50:01 -0300

    modificando archivos

--- webpage/src/models/Temas.js
+++ webpage/src/models/Temas.js
@@ -1,15 +1,17 @@
 export const temas =[
     {
-        'tema': 'Acordeón 1',
+        'tema': 'Sistema de gobierno',
+        'sinopsis': '',
         'descripcion': `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
         malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
         sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-        sit amet blandit leo lobortis eget.`,
-        'urlVideo': '',
+        sit amet bladit leo lobortis eget.`,
+        'urlVideo': <iframe width="100%" height="100%" src="https://www.youtube.com/embed/0uVOzkJB-QM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
         'panel':'panel1'
     },
     {
-        'tema': 'Acordeón 2',
+        'tema': 'Tipo de voto',
+        'sinopsis': '',
         'descripcion': `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
         malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
         sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
@@ -18,12 +20,23 @@ export const temas =[
         'panel':'panel2'
     },
     {
-        'tema': 'Acordeón 3',
+        'tema': '¿Que formas de voto existen?',
+        'sinopsis': '',
         'descripcion': `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
         malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
         sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
         sit amet blandit leo lobortis eget.`,
         'urlVideo': '',
         'panel':'panel3'
+    },
+    {
+        'tema': 'Confección del padrón',
+        'sinopsis': '',
+        'descripcion': `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+        sit amet blandit leo lobortis eget.`,
+        'urlVideo': '',
+        'panel':'panel4'
     },
 ]
--- webpage/src/routes/VideosPage.jsx
+++ webpage/src/routes/VideosPage.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { temas } from '../models/Temas'
+import '../Styles/Videos.scss'
 
-const VideosPage = ({video}) => {
+const VideosPage = () => {
+
+  const url = useParams('video')
+
+  const [video, setVideo] = useState('')
+
+
+  useEffect(() => {
+    FindVideo()
+  }, [])
+
+
+  const FindVideo = ()=>{
+
+
+    temas.map(tema=>{
+      if(tema.tema === url.video) return(setVideo(tema))
+    })
+
+  }
+
+
+
+
   return (
-    <div>
+    <section>
 
-      <div className='page__content'>
-          <p>some text</p>
+      <article className='page__content'>
+          <h2>{video.tema}</h2>
 
-          <div>
-            {video}
+          <div className='container video__container'>
+            {video.urlVideo}
           </div>
-      </div>
-    </div>
+      </article>
+      <article>
+        <p>{video.descripcion}</p>
+      </article>
+    </section>
   )
 }
 
